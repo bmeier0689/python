4320efba2a0107394bb608cb273a6f46dc965971
Brad Meier <bmeier0689@gmail.com>
2022-06-12 09:57:17 +0900

    week1 algo

---
+++ fundamentals/introduction/testing_python_and_algos.py
@@ -0,0 +1,71 @@
+import random
+
+print('Welcome to Python!')
+
+print('This is a loop printing 5 times')
+for x in range(1, 6):
+    print(f'x is: {x}')
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+day = random.choice(weekdays)
+
+print(f'Today is: {day}')
+
+if day == 'Monday':
+    print('It will be a long week!')
+elif(day == 'Friday'):
+    print('Woohoo, time for the weekend!')
+else:
+    print('Not quite there yet.')
+
+
+print(type(24))
+print(type(3.9))
+print(type(3j))
+
+int_to_float = float(35)
+float_to_int = int(44.2)
+int_to_complex = complex(35)
+print(int_to_float)
+print(float_to_int)
+print(int_to_complex)
+print(type(int_to_float))
+print(type(float_to_int))
+print(type(int_to_complex))
+
+
+import random
+print(random.randint(2,5)) # provides a random number between 2 and 5
+
+name = "Zen"
+print("My name is", 8)
+
+
+name = "Zen"
+print("My name is " + 8)
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
+
+list = [3,6,9,10]
+int = 3
+
+def divisible(listInput, intInput):
+    count = 0
+    for item in listInput:
+        if item % intInput == 0:
+            count += 1
+    return count
+
+print(divisible([3,6,9,10], 3))
