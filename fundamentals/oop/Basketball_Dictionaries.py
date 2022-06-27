c32d6066be68b1a6174b2468c17d50f2bbdb170b
Brad Meier <bmeier0689@gmail.com>
2022-06-27 20:29:50 +0900

    basketball

---
+++ fundamentals/oop/Basketball_Dictionaries.py
@@ -0,0 +1,91 @@
+class Player:
+    def __init__(self, data):
+        self.name = data['name']
+        self.age = data['age']
+        self.position = data['position']
+        self.team = data['team']
+
+    def __repr__(self):
+        display = f"Player: {self.name}, {self.age} years old, Pos: {self.position}, Team: {self.team}"
+        return display
+
+    @classmethod
+    def add_players(cls, data):
+        player_objects = []
+        for dict in data:
+            player_objects.append(cls(dict))
+        return player_objects
+
+kevin = {
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving", 
+    "age":32, "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+}
+
+player_kevin = Player(kevin)
+player_jason = Player(jason)
+player_kyrie = Player(kyrie)
+
+print(player_jason)
+print(player_kevin)
+print(player_kyrie)
+
+players = [
+    {
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+    },
+    {
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+    },
+    {
+    "name": "Kyrie Irving", 
+    "age":32, 
+    "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+    },
+    {
+    "name": "Damian Lillard", 
+    "age":33,
+    "position": "Point Guard", 
+    "team": "Portland Trailblazers"
+    },
+    {
+    "name": "Joel Embiid", 
+    "age":32, 
+    "position": "Power Foward", 
+    "team": "Philidelphia 76ers"
+    },
+    {
+    "name": "", 
+    "age":16, 
+    "position": "P", 
+    "team": "en"
+    }
+]
+
+new_team = []
+
+for player_dict in players:
+    player = Player(player_dict)
+    new_team.append(player)
+
+print(new_team)
+Player.add_players()
