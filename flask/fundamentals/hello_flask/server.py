93e13ca44e91ad9c5cd7957a7837518b4aac1e62
Brad Meier <bmeier0689@gmail.com>
2022-07-16 12:30:32 +0900

    templates practice

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', phrase = 'hello', times = 5)
 
 @app.route('/success')
 def success():
