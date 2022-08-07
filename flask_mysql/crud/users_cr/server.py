5404a5d07fa53532b39d98280ca299adc7e847e1
Brad Meier <bmeier0689@gmail.com>
2022-08-07 22:48:54 +0900

    users_cr

---
+++ flask_mysql/crud/users_cr/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+from users import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", all_users = users)
+
+@app.route('/new_user')
+def new_user():
+    return render_template('add_user.html')
+
+@app.route('/add_user', methods=["POST"])
+def add_user():
+    data = {
+        "fname": request.form['fname'],
+        "lname": request.form['lname'],
+        "email": request.form['email']
+    }
+    User.save(data)
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
