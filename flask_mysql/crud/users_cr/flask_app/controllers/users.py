f793f91f122c28a9fb997ddaeea15d778f802db5
Brad Meier <bmeier0689@gmail.com>
2022-08-14 17:26:37 +0900

    MVC update

---
+++ flask_mysql/crud/users_cr/flask_app/controllers/users.py
@@ -0,0 +1,49 @@
+from flask_app import app
+from flask import render_template, request, redirect
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", all_users = users)
+
+@app.route('/new_user')
+def new_user():
+    return render_template('add_user.html')
+
+@app.route('/add_user', methods=["POST"])
+def add_user():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/')
+
+@app.route('/user/<int:id>')
+def one_user(id):
+    data = {
+        'id': id
+    }
+    user = User.get_one_user(data)[0]
+    return render_template("one_user.html", user = user)
+
+@app.route('/edit_user/<int:id>')
+def edit_user(id):
+    data = {
+        'id': id
+    }
+    user = User.get_one_user(data)[0]
+    return render_template('/edit_user.html', user = user)
+
+@app.route('/update', methods=["POST"])
+def update_user():
+    print(request.form)
+    User.update_user(request.form)
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    data = {
+        'id': id
+    }
+    User.delete_user(data)
+    return redirect('/')
--- flask_mysql/crud/users_cr/server.py
+++ flask_mysql/crud/users_cr/server.py
@@ -1,53 +1,6 @@
-from flask import Flask, render_template, request, redirect
-from users import User
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    users = User.get_all()
-    print(users)
-    return render_template("index.html", all_users = users)
-
-@app.route('/new_user')
-def new_user():
-    return render_template('add_user.html')
-
-@app.route('/add_user', methods=["POST"])
-def add_user():
-    print(request.form)
-    User.save(request.form)
-    return redirect('/')
-
-@app.route('/user/<int:id>')
-def one_user(id):
-    data = {
-        'id': id
-    }
-    user = User.get_one_user(data)[0]
-    return render_template("one_user.html", user = user)
-
-@app.route('/edit_user/<int:id>')
-def edit_user(id):
-    data = {
-        'id': id
-    }
-    user = User.get_one_user(data)[0]
-    return render_template('/edit_user.html', user = user)
-
-@app.route('/update', methods=["POST"])
-def update_user():
-    print(request.form)
-    User.update_user(request.form)
-    return redirect('/')
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    data = {
-        'id': id
-    }
-    User.delete_user(data)
-    return redirect('/')
+from flask import render_template, request, redirect
+from flask_app import app
+from flask_app.controllers.users import User
 
 if __name__ == "__main__":
     app.run(debug=True)
